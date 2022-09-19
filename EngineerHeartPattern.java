fa3a5016c3373185387d27b5159f61610f276a7c
gukiruthika <112635198+gukiruthika@users.noreply.github.com>
2022-09-19 20:45:31 +0530

    Add files via upload

--- EngineerHeartPattern.java
+++ EngineerHeartPattern.java
@@ -28,42 +28,42 @@ rEngineerEngineerEngineerEngineerEngineerEngi
 	*/
 	public static void main(String[] args) {
 
-		String e = "neerEngi";
+		String e = "Engineer";
 		char[] ar = e.toCharArray();
-		int k = 0;
-		char[][] mat = new char[23][49];
+		int k = 4;
+		char[][] base = new char[23][49];
 		for (int i = 0; i < 23; i++) {
 			for (int j = 0; j < 49; j++) {
 				if (k > e.length() - 1)
 					k = 0;
-				mat[i][j] = ar[k];
+				base[i][j] = ar[k];
 				k++;
 			}
 		}
-		int a = 10, b = 0;
+		int space = 10, variation = 0;
 		for (int i = 0; i < 23; i++) {
 			for (int j = 2; j < 47; j++) {
-				if (j >= a && j < (49 - a)) {
+				if (j >= space && j < (49 - space)) {
 					if ((i == 0 && j > 18 && j < 30) || (i == 1 && j > 22 && j < 26))
 						System.out.print(" ");
 					else
-						System.out.print(mat[i][j]);
+						System.out.print(base[i][j]);
 				} else
 					System.out.print(" ");
 
 			}
 			if (i == 0)
-				b -= 4;
+				variation -= 4;
 			if (i == 1 || i == 12)
-				b += 2;
+				variation += 2;
 			if (i == 21)
-				b -= 2;
+				variation -= 2;
 			if (i == 2 || i == 4 || i == 9 || i == 15 || i == 18)
-				b++;
+				variation++;
 			if (i == 11 || i == 13 || i == 22)
-				b--;
+				variation--;
 
-			a += b;
+			space += variation;
 
 			System.out.println();
 		}
